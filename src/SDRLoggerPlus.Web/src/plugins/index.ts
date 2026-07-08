4df53f3517c7c4bb523bd9fac7bcc7d3cda9560d
Brent Crier <bcrier@gmail.com>
2026-07-08 07:22:16 -0500

    revert: restore the 2D Map panel (reverts dafed9f)
    User request: bring the leaflet 2D map back alongside the 3D globe.
    Reconciled with drift since June 14 where needed.

--- src/SDRLoggerPlus.Web/src/plugins/index.ts
+++ src/SDRLoggerPlus.Web/src/plugins/index.ts
@@ -1,6 +1,7 @@
 export { LogEntryPlugin } from './LogEntryPlugin';
 export { LogHistoryPlugin } from './LogHistoryPlugin';
 export { ClusterPlugin } from './ClusterPlugin';
+export { MapPlugin } from './MapPlugin';
 export { RotatorPlugin } from './RotatorPlugin';
 export { GlobePlugin } from './GlobePlugin';
 export { AntennaGeniusPlugin } from './AntennaGeniusPlugin';
